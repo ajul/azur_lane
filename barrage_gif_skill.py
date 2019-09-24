179dd18f59e42f3ce427cb3d19ad1ec68e57453a
Albert Julius Liu <ajul1987@gmail.com>
2019-09-23 22:29:09 -0700

    working skill barrage

--- barrage_gif_skill.py
+++ barrage_gif_skill.py
@@ -1,18 +1,32 @@
 import barrage_gif
 import load_lua
 import sys
+import traceback
 
 skill_data_srcs = load_lua.load_sharecfg('skill_data_template', key_type=int)
+ship_data_srcs = load_lua.load_sharecfg('ship_data_template', key_type=int)
 
 range_limit = 80
-max_duration = 5.0
-pad_duration = 0.25
+max_duration = 10.0
+pad_duration = 0.5
 
-world_size = (75, 40)
+world_size = (75, 50)
 # pixels per in-game unit
 ppu = 10
 
+# compute skill -> ship
+skill_to_ship_names_map = {}
+
+for ship_id, ship_data_src in ship_data_srcs.items():
+    for skill_id in ship_data_src['buff_list_display'].values():
+        if skill_id not in skill_to_ship_names_map:
+            skill_to_ship_names_map[skill_id] = []
+        if ship_data_src['name'] not in skill_to_ship_names_map[skill_id]:
+            skill_to_ship_names_map[skill_id].append(ship_data_src['name'])
+        
+
 for skill_id, skill_data_src in skill_data_srcs.items():
+    #if skill_id not in [10350, 29062, 29092]: continue
     try:
         skill_src = load_lua.load_skill(skill_id)
     except FileNotFoundError:
@@ -31,12 +45,16 @@ for skill_id, skill_data_src in skill_data_srcs.items():
         if effect['type'] != 'BattleSkillFire': continue
         weapon_ids.append(effect['arg_list']['weapon_id'])
     if len(weapon_ids) == 0: continue
-    print(skill_data_src['name'], skill_id, ':', weapon_ids)
+    ship_name = ''
+    if skill_id in skill_to_ship_names_map:
+        ship_names = skill_to_ship_names_map[skill_id]
+    print('ships', ship_names, ':', skill_data_src['name'], 'skill_id', skill_id, ': weapon_ids', weapon_ids)
     filename_out = 'skill_gif_out/bullet_pattern_skill_%d.gif' % skill_id
     try:
         barrage_gif.create_barrage_gif(filename_out, weapon_ids, world_size, ppu,
-                                       range_limit = range_limit, max_duration = max_duration, pad_duration = pad_duration)
+                                       range_limit = range_limit, max_duration = max_duration, pad_duration = pad_duration,
+                                       color_count = 32)
     except:
-        print("Unexpected error:", sys.exc_info())
+        print(sys.exc_info(), traceback.print_exc())
     
 
