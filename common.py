4b4d9664f6cf7d50529620bbf32e2f8e13f4f82f
Albert Julius Liu <ajul1987@gmail.com>
2019-11-24 14:41:46 -0800

    common defs

---
+++ common.py
@@ -0,0 +1,40 @@
+attributes = {
+    1 : 'Health',
+    2 : 'Firepower',
+    3 : 'Torpedo',
+    4 : 'AA',
+    5 : 'Aviation',
+    6 : 'Reload',
+    8 : 'Accuracy',
+    9 : 'Evasion',
+    12 : 'ASW',
+}
+
+ship_types = {
+    1 : 'DD',
+    2 : 'CL',
+    3 : 'CA',
+    4 : 'BC',
+    5 : 'BB',
+    6 : 'CVL',
+    7 : 'CV',
+    8 : 'SS',
+    10 : 'BBV',
+    12 : 'AR',
+    13 : 'BM',
+    14 : 'Torpedo',
+    15 : 'Cargo',
+    16 : 'Bomb',
+}
+
+aircraft_types = {
+    1 : 'Fighter',
+    2 : 'Torpedo Bomber',
+    3 : 'Dive Bomber',
+}
+
+armor_types = {
+    1 : 'Light',
+    2 : 'Medium',
+    3 : 'Heavy',
+}
